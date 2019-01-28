2cf20e4a1c8ee56ca1fe83574665024b1e4dda1f
Sheik <udooz@hotmail.com>
2019-01-28 23:12:10 +0530

    CNN Introduction updated

--- 04_1_CNN_Intro.py
+++ 04_1_CNN_Intro.py
@@ -99,3 +99,91 @@ y_train
 
 #%%
 np.unique(y_train)
+
+#%% [markdown]
+# In order to define a fully connected network, the shape of the label array should matches the output of the network
+# the output is 10 output.
+# We can use to_categorical()
+
+#%%
+from keras.utils.np_utils import to_categorical
+
+#%%
+y_train_cat = to_categorical(y_train)
+y_test_cat = to_categorical(y_test)
+print(y_train[0])
+print(y_train_cat[0])
+print(y_train_cat.shape)
+print(y_test_cat.shape)
+
+#%% [markdown]
+# # Building the CNN
+# Input: 784 features
+# Layer 1: 512 nodes with Relu activation
+# Layer 2: 256 nodes with Relu
+# Layer 3: 128 nodes with Relu
+# Layer 4: 32 nodes with Relu
+# Output: 10 nodes with Softmax activation
+
+#%%
+from keras.models import Sequential
+from keras.layers import Dense
+
+#%%
+model = Sequential()
+model.add(Dense(512, input_dim=784, activation='relu'))
+model.add(Dense(256, activation='relu'))
+model.add(Dense(128, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(10, activation='softmax'))
+
+model.compile(loss='categorical_crossentropy',
+    optimizer='rmsprop',
+    metrics=['accuracy'])
+
+model.summary()
+
+#%% [markdown]
+# Let's train for 10 epochs with 128 images per batch
+
+#%%
+h = model.fit(X_train_sc, y_train_cat, batch_size=128,
+    epochs=10, verbose=1, validation_split=0.1)
+
+#%%
+plt.plot(h.history['acc'])
+plt.plot(h.history['val_acc'])
+plt.legend(['Training', 'Validation'])
+plt.title('Accuracy')
+plt.xlabel('Epochs')
+
+#%% [markdown]
+# The above chart shows validation does not perform well
+# Let us evaluate the performance on test set
+
+#%%
+test_acc = model.evaluate(X_test_sc, y_test_cat)[1]
+test_acc
+
+#%%
+train_acc = model.evaluate(X_train_sc, y_train_cat)[1]
+train_acc
+
+#%%
+y_pred_cat = model.predict(X_test_sc)
+y_pred = np.argmax(y_pred_cat, axis=-1)
+
+#%%
+print(y_pred[:5])
+print(y_test[:5])
+
+#%% [markdown]
+# Train is better than test, means overfitting
+# In this context, we need to provide better features from the images
+# Any better way to extract features from images
+# This process is called as feature extraction.
+#
+# ## Feature Extraction
+# * Fourier transforms
+# * Wavelet transforms
+# * Histograms of oriented gradients (HOG)
